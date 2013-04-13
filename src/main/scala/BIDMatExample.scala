2e94ae5b684e9a050c9a4570a8b1604cac543c7d
derrickcheng <derrick@eecs.berkeley.edu>
2013-04-12 19:48:21 -0700

    removed exp

--- src/main/scala/BIDMatExample.scala
+++ src/main/scala/BIDMatExample.scala
@@ -37,7 +37,7 @@ object BIDMatExample extends Configured with Tool {
 		var matIO : MatIO = new MatIO()
 		//matI0
 		matIO.mat = fMat
-		exp(fMat)
+		//exp(fMat)
 		context.write(new LongWritable(0), matIO)
     }
   }
